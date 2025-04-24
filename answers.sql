a7902b696af4626174452261835328319f446e8f
Rommedahl Magana <rommymagana5@gmail.com>
2025-04-24 20:00:18 +0300

    RPM commit

---
+++ answers.sql
@@ -0,0 +1,23 @@
+
+-- QUESTION 1
+    CREATE TABLE ProductDetails (
+        OrderID INT PRIMARY KEY,
+        CustomerName VARCHAR(50),
+        Product VARCHAR(50),
+        Quantity INT
+    );
+
+-- QUESTION 2
+    -- Customer Details Table 
+    CREATE TABLE CustomerDetails (
+        OrderID INT PRIMARY KEY,
+        CustomerName VARCHAR(50),
+    );
+
+    -- Order Details Table
+    CREATE TABLE OrderDetails (
+        OrderID INT PRIMARY KEY,
+        Product VARCHAR(50),
+        Quantity INT,
+        FOREIGN KEY (OrderID) REFERENCES CustomerDetails(OrderID)
+    );
